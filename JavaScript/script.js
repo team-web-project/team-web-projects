64fb0e75e086fc5594033efb27e1f9ef06895323
reucloud <yuma20040824@icloud.com>
2025-09-30 18:35:58 +0900

    Delete JavaScript/script.js

--- JavaScript/script.js
+++
@@ -1,8 +0,0 @@
-const fortune = document.getElementById("draw");
-const result = document.getElementById("result");
-
-const fortuneslip = ["大吉", "中吉", "小吉", "吉", "末吉", "半吉", "凶"];
-fortune.addEventListener("click", () => {
-  const random = Math.floor(Math.random() * 7);
-  result.textContent = fortuneslip[random];
-});
